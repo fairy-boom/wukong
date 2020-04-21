9829be7165d52280f15a629234fda06375393d36
tookbra <tookbra@outlook.com>
2020-04-21 19:12:06 +0800

    :sparkles: mybatis plus configuration

---
+++ wukong-common/wukong-mybatis-starter/src/main/java/org/okboom/wukong/mybatis/base/BaseDomain.java
@@ -0,0 +1,35 @@
+package org.okboom.wukong.mybatis.base;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableLogic;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+/**
+ * @author tookbra
+ */
+public class BaseDomain implements Serializable {
+    private static final long serialVersionUID = 8354402933053978830L;
+
+
+    /**
+     * 主键
+     */
+    @TableId(value = "id", type = IdType.ASSIGN_ID)
+    private Long id;
+    /**
+     * 创建人
+     */
+    private Long createUser;
+    /**
+     * 创建时间
+     */
+    private LocalDateTime createTime;
+    /**
+     * 状态[0:未删除,1:删除]
+     */
+    @TableLogic
+    private Integer deleted;
+}
---
+++ wukong-common/wukong-mybatis-starter/src/main/java/org/okboom/wukong/mybatis/base/BaseService.java
@@ -0,0 +1,10 @@
+package org.okboom.wukong.mybatis.base;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * 基础业务接口
+ * @author tookbra
+ */
+public interface BaseService<T> extends IService<T> {
+}
