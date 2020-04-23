99bc11e2aece6c1950576e6c093c1075265294eb
tookbra <tookbra@outlook.com>
2020-04-23 21:40:50 +0800

    :sparkles: getter setter

--- wukong-common/wukong-mybatis-starter/src/main/java/org/okboom/wukong/mybatis/base/BaseDomain.java
+++ wukong-common/wukong-mybatis-starter/src/main/java/org/okboom/wukong/mybatis/base/BaseDomain.java
@@ -3,6 +3,7 @@ package org.okboom.wukong.mybatis.base;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableLogic;
+import lombok.Data;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
@@ -10,6 +11,7 @@ import java.time.LocalDateTime;
 /**
  * @author tookbra
  */
+@Data
 public class BaseDomain implements Serializable {
     private static final long serialVersionUID = 8354402933053978830L;
 
