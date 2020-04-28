f965d7199f710006378662a1e68f8d112ff17959
tookbra <tookbra@outlook.com>
2020-04-28 22:29:25 +0800

    :sparkles: add user controller

---
+++ wukong-service/wukong-user/wukong-user-service/src/main/java/org/okboom/wukong/user/controller/UserController.java
@@ -0,0 +1,10 @@
+package org.okboom.wukong.user.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author tookbra
+ */
+@RestController("/user")
+public class UserController {
+}
